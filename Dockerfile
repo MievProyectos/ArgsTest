4832bd4b4d9181ab3c993101bb03f290d7d3514b
Isai Castro <isa.734@hotmail.com>
2021-07-14 11:43:41 -0500

    Add ENV vars

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM openjdk:8-jdk
+ENV DB_USER = "icastro"
+ENV DB_NAME = "mievdatabase"
 RUN mkdir /app
 COPY ./build/install/ArgsTest/ /app/
 ENTRYPOINT ["java","-jar","/app/lib/ArgsTest-1.0.jar"]
--- src/Application.kt
+++ src/Application.kt
@@ -1,3 +1,5 @@
 fun main(args: Array<String>){
     println(args.toList().toString())
+    println("DB_USER --> "+System.getenv()["DB_USER"])
+    println("DB_NAME --> "+System.getenv()["DB_NAME"])
 }
